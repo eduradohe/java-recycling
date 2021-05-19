215a0143f7e9d025bae53718af7ac87d716a4db3
Eduardo Oliveira <eduardohe@gmail.com>
2021-05-19 13:03:19 +0200

    Person class implementation

---
+++ src/edu/plural/learn/model/Person.java
@@ -0,0 +1,35 @@
+package edu.plural.learn.model;
+
+public class Person {
+
+    private String name;
+    private Integer age;
+
+    public Person() {
+        super();
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
--- src/edu/plural/learn/util/GenericBuilder.java
+++ src/edu/plural/learn/util/GenericBuilder.java
@@ -1,7 +1,6 @@
 package edu.plural.learn.util;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
