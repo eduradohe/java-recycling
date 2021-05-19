0b59b70659e9bd0abff44f286661297e60d726d7
Eduardo Oliveira <eduardohe@gmail.com>
2021-05-19 13:04:13 +0200

    Testing maps, predicates and chaining of consumers

--- src/edu/plural/learn/functions/ChainConsumers.java
+++ src/edu/plural/learn/functions/ChainConsumers.java
@@ -1,12 +1,36 @@
 package edu.plural.learn.functions;
 
+import edu.plural.learn.model.Person;
+import edu.plural.learn.util.GenericBuilder;
+
 import java.util.*;
 import java.util.function.BiPredicate;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class ChainConsumers {
-    public static void main(String[] args) {
+
+    private static void chainPersons() {
+
+        final List<Person> persons = Arrays.asList(
+                GenericBuilder.of(Person::new).with(Person::setName, "Eduardo").with(Person::setAge, 33).build(),
+                GenericBuilder.of(Person::new).with(Person::setName, "Karolina").with(Person::setAge, 30).build(),
+                GenericBuilder.of(Person::new).with(Person::setName, "Rafal").with(Person::setAge, 5).build(),
+                GenericBuilder.of(Person::new).with(Person::setName, "Rafaela").with(Person::setAge, 18).build(),
+                GenericBuilder.of(Person::new).with(Person::setName, "Thiago").with(Person::setAge, 25).build()
+        );
+
+        final List<String> result = persons.stream()
+                .filter(p -> (p.getAge() > 20 && p.getAge() < 30))
+                .map(p -> "[" + p.getName() + ", " + p.getAge().toString() + "]")
+                .collect(Collectors.toList());
+
+        result.forEach(System.out::println);
+    }
+
+    private static void chainSimpleStrings() {
 
         final List<String> strings = Arrays.asList("one", "two", "three", "four", "five");
         final List<String> result = new ArrayList<>();
@@ -25,5 +49,10 @@ public class ChainConsumers {
             System.out.println("Result has the same elements as Strings list!");
             System.out.println("Result list size: " + result.size());
         }
+    }
+
+    public static void main(String[] args) {
+        chainSimpleStrings();
+        chainPersons();
     }
 }
